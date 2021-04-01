3036a1d52b76d0e86b32508ecc5e539be5d0e497
Juan Cuzmar <jcuzmar@protonmail.com>
2021-03-31 23:04:05 -0300

    first commit

---
+++ .scripts/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+nitrogen --restore &
+(sleep 1 && picom -b) &
+#(sleep 1 && pulseaudio -k) &
+#(sleep 1 && trayer --edge bottom --align right --SetDockType true --SetPartialStrut true --expand true --width 6 --height 18 --transparent true --alpha 0 --tint 0x000000 --expand true --iconspacing 2 --margin 4 --distance 4) &
+#(sleep 1 && connman-ui-gtk) &
+#(sleep 1 && caffeine) &
+#(sleep 1 && blueman-applet) &
+#(sleep 1 && parcellite) &
+#(sleep 1 && pkill -HUP spectrwm) &
